eaec05d537d7d2c06f144dec17dc8f8a2d7d1527
Xiaoxia <jinxiaoxiaclaire@gmail.com>
2024-11-25 23:15:22 +0000

    Update project showcase with new subfolder structure
    Updated the JavaScript code in the homepage to link to the new subfolder containing the project files. The project is now located in the 'pomodoro-clock' folder.

--- script.js
+++ script.js
@@ -1,84 +1,53 @@
-const breakLength = document.getElementById("break-length");
-const sessionLength = document.getElementById("session-length");
-const timeLeft = document.getElementById("time-left");
-const timerLabel = document.getElementById("timer-label");
-const startStop = document.getElementById("start_stop");
-const beep = document.getElementById("beep");
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("container");
+  
 
-let timeLeftValue = 25 * 60;
-let isTimerStop = true;
-let timerInterval;
-
-const formatTime = (time) => {
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    timeLeft.className = minutes < 1 ? "redColor" : "";
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    timeLeft.innerText = `${minutes}:${seconds}`;
-};
-
-
-const increment = (label, lengthElement) => {
-    let time = Number(lengthElement.innerText);
-    if (isTimerStop && time < 60) {
-        time += 1;
-        lengthElement.innerText = time;
-        timeLeftValue = timerLabel.innerText === label ? time * 60 : timeLeftValue;
-        formatTime(timeLeftValue);
-    }
-};
-
-const decrement = (label, lengthElement) => {
-    let time = Number(lengthElement.innerText);
-    if (isTimerStop && time > 1) {
-        time -= 1;
-        lengthElement.innerText = time;
-        timeLeftValue = timerLabel.innerText === label ? time * 60 : timeLeftValue;
-        formatTime(timeLeftValue);
-    }
-};
-
-const timerToggle = () => {
-    if (isTimerStop) {
-        startStop.innerText = "pause";
-    } else {
-        startStop.innerText = "play_arrow";
-    }
-    isTimerStop = !isTimerStop;
-    if (isTimerStop) {
-        clearInterval(timerInterval);
-    } else {
-        timerInterval = setInterval(() => {
-            if (timeLeftValue > 0) {
-                timeLeftValue -= 1;
-            } else {
-                beep.currentTime = 0;
-                beep.play();
-                isTimerStop = true;
-                if (timerLabel.innerText === "Session") {
-                    timerLabel.innerText = "Break";
-                    timeLeftValue = Number(breakLength.innerText) * 60;
-                } else {
-                    timerLabel.innerText = "Session";
-                    timeLeftValue = Number(sessionLength.innerText) * 60;
-                }
-            }
-            formatTime(timeLeftValue);
-        }, 1000);
-    };
-
-}
-const resetTimer = () => {
-    clearInterval(timerInterval);
-    isTimerStop = true;
-    breakLength.innerText = 5;
-    sessionLength.innerText = 25;
-    timerLabel.innerText = "Session";
-    timeLeftValue = 25 * 60;
-    startStop.innerText = "play_arrow";
-    formatTime(timeLeftValue);
-    beep.pause();
-    beep.currentTime = 0;
-};
+    const projects = [
+      {
+        imageUrl: "./assets/pomodoro.png",
+        title: "Pomodoro Clock",
+        keywords: "HTML, CSS, JavaScript",
+        url: "https://xiaoxiajin.github.io/pomodoro-clock/index.html",
+      },
+      {
+        imageUrl: "./assets/project2.png",
+        title: "Roman numeral convert",
+        keywords: "Python, Flask, SQL",
+        url: "https://xiaoxiajin.github.io/roman-numeral-convert/index.html",
+      },
+    ];
+  
+    //dynamically create cards
+    projects.forEach(function (project) {
+      const card = document.createElement("div");
+      card.classList.add("card");
+  
+      const img = document.createElement("img");
+      img.src = project.imageUrl;
+      img.alt = project.title;
+  
+      const cardContent = document.createElement("div");
+      cardContent.classList.add("card-content");
+  
+      const h3 = document.createElement("h3");
+      h3.textContent = project.title;
+  
+      const p = document.createElement("p");
+      p.textContent = project.keywords;
+  
+      const link = document.createElement("a");
+      link.href = project.url;
+      link.textContent = "View more";
+      link.classList.add("view-more-btn");
+      link.target = "_blank";
+  
+      cardContent.appendChild(h3);
+      cardContent.appendChild(p);
+      cardContent.appendChild(link);
+      card.appendChild(img);
+      card.appendChild(cardContent);
+  
+      container.appendChild(card);
+    });
+  });
+  
